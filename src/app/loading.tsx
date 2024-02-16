1301a36fc9525fe0144b1e1eac1dec65f9510b30
eung-eung <boyvip5231@gmail.com>
2024-02-16 17:37:56 +0700

    add loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import LoadingTheme from './components/loadingTheme/loadingTheme'
+
+export default function loading() {
+    return (
+        <div><LoadingTheme /></div>
+    )
+}
