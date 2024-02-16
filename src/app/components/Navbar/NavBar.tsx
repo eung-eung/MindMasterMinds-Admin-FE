af4749a753514d0ac80ef4a35fa80e8249df4209
eung-eung <boyvip5231@gmail.com>
2024-02-16 17:52:20 +0700

    style width nav

--- src/app/components/Navbar/NavBar.tsx
+++ src/app/components/Navbar/NavBar.tsx
@@ -22,54 +22,58 @@ export default function Navbar() {
     return (
         <>
             {!session && status === 'unauthenticated' ? '' :
-                <div className={classes.nav_links}>
-                    <div className={classes.user_container}>
-                        <img className={classes.avatar} src='/images/icon.jpg' />
-                        <div className={classes.user_detail}>
-                            <p>{session?.user.userViewLogin.firstName
-                                + ' '
-                                + session?.user.userViewLogin.lastName}
-                            </p>
-                            <p>{session?.user.userViewLogin.userRole.roleName}</p>
+                <>
+                    <div className={classes.nav_links}>
+                        <div className={classes.user_container}>
+                            <img className={classes.avatar} src={session?.user.userViewLogin.avatar} />
+                            <div className={classes.user_detail}>
+                                <p>{session?.user.userViewLogin.firstName
+                                    + ' '
+                                    + session?.user.userViewLogin.lastName}
+                                </p>
+                                <p className={classes.role}>{session?.user.userViewLogin.userRole.roleName}</p>
+                            </div>
                         </div>
+                        <Link
+                            className={router == '/dashboard' || router == '/' ? classes.active : ''}
+                            href="/dashboard">
+                            <DashboardIcon
+                                className={classes.nav_icon}
+                            /> Dashboard
+                        </Link>
+
+                        <Link
+                            className={router == '/student' ? classes.active : ''}
+                            href="/student"
+                        >
+                            <AccountCircleIcon
+                                className={classes.nav_icon}
+                            />
+                            Student
+                        </Link>
+
+                        <Link
+                            className={router == '/tutor' ? classes.active : ''}
+                            href="/tutor">
+                            <CameraIcon className={classes.nav_icon}
+                            />
+                            Tutor
+                        </Link>
+
+                        <Link
+                            className={router == '/post' ? classes.active : ''}
+                            href="/post">
+                            <ListAltIcon className={classes.nav_icon}
+                            />
+                            Post
+                        </Link>
+                        <div className={classes.logout} onClick={handleLogOut}>
+                            <LogoutIcon className={classes.nav_icon} /> Logout
+                        </div>
+                        <img className={classes.logo} src="/images/icon.jpg" />
                     </div>
-                    <Link
-                        className={router == '/dashboard' || router == '/' ? classes.active : ''}
-                        href="/dashboard">
-                        <DashboardIcon
-                            className={classes.nav_icon}
-                        /> Dashboard
-                    </Link>
 
-                    <Link
-                        className={router == '/student' ? classes.active : ''}
-                        href="/student"
-                    >
-                        <AccountCircleIcon
-                            className={classes.nav_icon}
-                        />
-                        Student Management
-                    </Link>
-
-                    <Link
-                        className={router == '/tutor' ? classes.active : ''}
-                        href="/tutor">
-                        <CameraIcon className={classes.nav_icon}
-                        />
-                        Tutor Management
-                    </Link>
-
-                    <Link
-                        className={router == '/post' ? classes.active : ''}
-                        href="/post">
-                        <ListAltIcon className={classes.nav_icon}
-                        />
-                        Post Management
-                    </Link>
-                    <div className={classes.logout} onClick={handleLogOut}>
-                        <LogoutIcon className={classes.nav_icon} /> Logout
-                    </div>
-                </div>
+                </>
             }
 
 
