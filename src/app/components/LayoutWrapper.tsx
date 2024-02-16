4ce3b02be828bed6c3571b784232836351e4ff94
eung-eung <boyvip5231@gmail.com>
2024-02-16 17:58:03 +0700

    style

--- src/app/components/LayoutWrapper.tsx
+++ src/app/components/LayoutWrapper.tsx
@@ -28,7 +28,7 @@ export default function LayoutWrapper({
                 {
                     isLoading ? <LoadingTheme /> : <>
                         <Navbar />
-                        <div style={{ marginLeft: '20%', paddingLeft: '5%' }}>
+                        <div style={{ marginLeft: '15%', paddingLeft: '5%' }}>
                             {children}
                         </div>
                     </>
