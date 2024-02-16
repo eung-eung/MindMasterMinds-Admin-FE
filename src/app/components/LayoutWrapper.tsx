51fcf4142d7b1b031c1ba9fd273dd4f9debd1ee8
eung-eung <boyvip5231@gmail.com>
2024-02-16 17:38:32 +0700

    create layout wrapper

---
+++ src/app/components/LayoutWrapper.tsx
@@ -0,0 +1,40 @@
+
+'use client'
+import { useSession } from 'next-auth/react'
+import Navbar from './Navbar/NavBar'
+import { useEffect, useState } from 'react'
+import LoadingTheme from './loadingTheme/loadingTheme'
+
+
+
+
+export default function LayoutWrapper({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const { data: session, status } = useSession()
+    const [isLoading, setLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        if (status !== 'loading') setLoading(false)
+    }, [status])
+    return (
+        <html lang="en">
+            <head>
+
+            </head>
+            <body>
+                {
+                    isLoading ? <LoadingTheme /> : <>
+                        <Navbar />
+                        <div style={{ marginLeft: '20%', paddingLeft: '5%' }}>
+                            {children}
+                        </div>
+                    </>
+                }
+
+            </body>
+        </html>
+    )
+}
