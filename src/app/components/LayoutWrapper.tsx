b434121122ce1618f905b6db5efe1423e5256a3a
eung-eung <boyvip5231@gmail.com>
2024-03-17 17:52:34 +0700

    fix style

--- src/app/components/LayoutWrapper.tsx
+++ src/app/components/LayoutWrapper.tsx
@@ -28,7 +28,7 @@ export default function LayoutWrapper({
                 {
                     isLoading ? <LoadingTheme /> : <>
                         <Navbar />
-                        <div style={{ marginLeft: '15%', paddingLeft: '5%', backgroundColor:"#F3F5F9" }}>
+                        <div style={{ marginLeft: '15%', backgroundColor: "#F3F5F9" }}>
                             {children}
                         </div>
                     </>
--- src/app/components/OrderPage/page.tsx
+++ src/app/components/OrderPage/page.tsx
@@ -3,9 +3,9 @@ import TableOrder from './TableOrder'
 
 export default function OrderManage() {
   return (
-    <div className='container mx-auto' style={{paddingTop: "60px", paddingBottom: "30vh", paddingRight:"100px"  }}>
+    <div className='container mx-auto' style={{ paddingTop: "60px", paddingBottom: "30vh" }}>
       <h1 className='flex justify-center font-semibold text-4xl pb-12 font-[Belanosima] text-gray-700'>Order Management</h1>
-      <TableOrder/>
+      <TableOrder />
     </div>
   )
 }
